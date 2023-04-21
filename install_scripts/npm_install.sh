14064e46b2b795dbdb7b79ed33e02e8e8b2f65c5
owlsly94 <stefan.stanimirovic09@gmail.com>
2023-04-21 22:03:22 +0200

    npm

--- install_scripts/npm_install.sh
+++ install_scripts/npm_install.sh
@@ -1,1 +1,17 @@
+# Python server
 sudo npm i -g pyright
+
+# HTML server
+sudo npm i -g vscode-langservers-extracted
+
+# VS Code
+sudo npm i -g vscode-langservers-extracted
+
+# BASH server
+sudo npm i -g bash-language-server
+
+# Vue server
+sudo npm install -g @volar/vue-language-server
+
+# Typescript server
+sudo npm install -g typescript typescript-language-server
